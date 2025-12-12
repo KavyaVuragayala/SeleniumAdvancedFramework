524fefc6cdf652125f1e098b27f4f171729ac9f4
KavyaVuragayala <vura.kavya18@gmail.com>
2025-12-12 14:08:23 +0530

    100% Framework

--- src/main/java/com/kavya/pages/pageFactory/vwo/DashboardPage_PF.java
+++ src/main/java/com/kavya/pages/pageFactory/vwo/DashboardPage_PF.java
@@ -1,4 +1,34 @@
 package com.kavya.pages.pageFactory.vwo;
 
-public class DashboardPage_PF {
+import com.kavya.base.CommonToAllPage;
+import com.kavya.utils.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage_PF extends CommonToAllPage {
+
+    WebDriver driver;
+    public DashboardPage_PF(WebDriver driver){
+
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    //private By userNameDashBoard = By.xpath("//h6");
+    @FindBy(xpath = "//h6")
+    private By userNameDashBoard;
+
+    public String loggedInUsername(){
+
+        WaitHelpers.waitJVM(5000);
+        openVWODashBoardUrl();
+        String username= getText(userNameDashBoard);
+        return username;
+
+//        WaitHelpers.visibilityOfElement(userNameDashBoard);
+//        return getText(userNameDashBoard);
+
+    }
 }
--- src/main/java/com/kavya/pages/pageFactory/vwo/LoginPage_PF.java
+++ src/main/java/com/kavya/pages/pageFactory/vwo/LoginPage_PF.java
@@ -1,4 +1,47 @@
 package com.kavya.pages.pageFactory.vwo;
 
-public class LoginPage_PF {
+import com.kavya.base.CommonToAllPage;
+import com.kavya.utils.PropertiesReader;
+import com.kavya.utils.WaitHelpers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage_PF extends CommonToAllPage {
+
+    WebDriver driver;
+    public LoginPage_PF(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(id="login-username")
+    private By username;
+
+    @FindBy(name = "password")
+    private By password;
+
+    @FindBy(id = "js-login-btn")
+    private WebElement signButton;
+
+    @FindBy(css = "#js-notification-box-msg")
+    private WebElement error_message;
+
+    public String loginToVWOInvalidCreds(){
+
+        try {
+            openVWOUrl();
+            enterInput(username, PropertiesReader.readKey("invalid_username"));
+            enterInput(password, PropertiesReader.readKey("invalid_password"));
+            clickElement(signButton);
+            WaitHelpers.waitJVM(5000);
+        } catch (Exception e) {
+            System.out.println("Elements Not found!");
+        }
+        return getText(error_message);
+
+
+    }
 }
